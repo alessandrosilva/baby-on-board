e0a313813063475c0738a01f7fecba0caef4896f
Alessandro Oliveira Silva <alessandrosilva@outlook.com>
2016-09-06 11:27:20 -0300

    Primeiro Versionamento com o site angular

---
+++ js/controllers/RepresentanteController.js
@@ -0,0 +1,23 @@
+app.controller('RepresentanteController', ['$scope', function($scope){
+
+
+ $scope.representantes = [
+	{
+		id_representante: 1,
+		nome: 'Alessandro Oliveira Silva',
+		funcao: 'Estoquista',
+		imei: '1237SDFN1235',
+		latitude: '0984230948,84',
+		longitude: '89457439857,47',
+		ativo: true
+	}
+	];
+
+
+
+
+   
+	
+
+}]);
+
